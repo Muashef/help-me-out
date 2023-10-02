f7d60405476196b1341fba63976388e9cb629fd6
Sherif <muhammedsherif40@yahoo.com>
2023-10-02 10:17:57 +0100

    updates

---
+++ public/popup.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const startRecordingButton = document.querySelector("button#start_recording")
+    const stopRecordingButton = document.querySelector("button#stop_recording")
+
+    startRecordingButton.addEventListener("click", ()=>{
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id, {action: "request_recording"}, function(response){
+                if(!chrome.runtime.lastError){
+                    console.log(response)
+                } else{
+                    console.log(chrome.runtime.lastError, 'error line 14')
+                }
+            })
+        })
+    })
+
+    stopRecordingButton.addEventListener("click", ()=>{
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+            chrome.tabs.sendMessage(tabs[0].id, {action: "stoprecording"}, function(response){
+                if(!chrome.runtime.lastError){
+                    console.log(response)
+                } else{
+                    console.log(chrome.runtime.lastError, 'error line 27')
+                }
+            })
+        })
+    })
+})
